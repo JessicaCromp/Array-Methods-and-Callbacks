62549d4ae34833063a6909eab0cc5c7536ee55f7
JessicaCromp <jessica.cromp@gmail.com>
2020-11-10 20:58:10 -0700

    first

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ import { fifaData } from './fifa.js';
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Investigate the data above. Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
+const finals2014 = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final';
+});
+console.log('finals2014 array:', finals2014);
 
 //(a) Home Team name for 2014 world cup final
-
+console.log('Task 1 a:', finals2014 [0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
-
+console.log('Task 1 a:', finals2014 [0]['Away Team Name']);
 //(c) Home Team goals for 2014 world cup final
 
 //(d) Away Team goals for 2014 world cup final
@@ -19,9 +23,14 @@ Investigate the data above. Practice accessing data by console.log-ing the follo
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(array) {
+   const onlyFinals = array.filter(function(item){
+       return item.Stage === "Final";
+   });
+    return onlyFinals;
 }
+
+   console.log(getFinals (fifaData));
 
 
 
